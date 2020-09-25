98a9289fab4de0961dc852f26d6e98bf810d07b1
Joel Hari <joel@familiehari.ch>
2020-09-25 23:51:13 +0200

    fixed desktop launcher

--- desktop/src/ch/joelstudios/minesweeper/desktop/DesktopLauncher.java
+++ desktop/src/ch/joelstudios/minesweeper/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package ch.joelstudios.minesweeper.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import ch.joelstudios.minesweeper.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Main(), config);
 	}
 }
